c6a270eac1e0a528dec5728614f6c73423ab6525
LengYuQi <lengyuqi@adesk.com>
2019-07-05 19:01:12 +0800

    ringbuffer

--- DataStructures/Queues.playground/Pages/RingBuffer.xcplaygroundpage/Contents.swift
+++ DataStructures/Queues.playground/Pages/RingBuffer.xcplaygroundpage/Contents.swift
@@ -2,6 +2,68 @@
 
 import Foundation
 
-var str = "Hello, playground"
+public struct RingBuffer<T> {
+    fileprivate var array: [T?]
+    fileprivate var readIndex = 0
+    fileprivate var writeIndex = 0
+    
+    public init(count: Int) {
+        array = [T?](repeating: nil, count: count)
+    }
+    
+    public mutating func write(_ element: T) -> Bool {
+        if !isFull {
+            array[writeIndex % array.count] = element
+            writeIndex += 1
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    public mutating func read() -> T? {
+        if !isEmpty {
+            let element = array[readIndex % array.count]
+            readIndex += 1
+            return element
+        } else {
+            return nil
+        }
+    }
+    
+    fileprivate var availableSpaceForReading: Int {
+        return writeIndex - readIndex
+    }
+    
+    public var isEmpty: Bool {
+        return availableSpaceForReading == 0
+    }
+    fileprivate var availableSpaceForWriting: Int {
+        return array.count - availableSpaceForReading
+    }
+    
+    public var isFull: Bool {
+        return availableSpaceForWriting == 0
+    }
+}
 
+
+// testing
+var buffer = RingBuffer<Int>(count: 5)
+buffer.write(123)
+buffer.write(456)
+buffer.write(789)
+buffer.write(666)
+
+buffer.read()
+buffer.read()
+buffer.read()
+//
+buffer.write(333)
+buffer.write(555)
+
+buffer.read()
+buffer.read()
+buffer.read()
+buffer.read()
 //: [Next](@next)
